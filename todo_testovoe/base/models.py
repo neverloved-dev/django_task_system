503922f693a765d548491fcc6d28b072da8e6466
WinterWiseTech <winterwisetech@gmail.com>
2025-03-06 18:18:39 +0100

    add model

--- todo_testovoe/base/models.py
+++ todo_testovoe/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    id = models.IntegerField("id",primary_key=True,unique=True)
+    title = models.CharField("title",max_length=100)
+    is_completed = models.BooleanField("is_completed",default=False)
