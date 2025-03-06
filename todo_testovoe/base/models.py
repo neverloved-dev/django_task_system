7ec699a2005590fd05a2e1da399640174df10fee
WinterWiseTech <winterwisetech@gmail.com>
2025-03-06 18:57:04 +0100

    add title field validation

--- todo_testovoe/base/models.py
+++ todo_testovoe/base/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     id = models.IntegerField("id",primary_key=True,unique=True)
-    title = models.CharField("title",max_length=100)
+    title = models.CharField("title",max_length=100,null=False,blank=False)
     is_completed = models.BooleanField("is_completed",default=False)
