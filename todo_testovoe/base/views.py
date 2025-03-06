568e7ff6174f45ff992fcfcc39d25e73f2076cbc
WinterWiseTech <winterwisetech@gmail.com>
2025-03-06 18:37:30 +0100

    add views

--- todo_testovoe/base/views.py
+++ todo_testovoe/base/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 # Create your views here.
+@api_view(['GET','POST'])
+def handle_tasks(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks,many = True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
